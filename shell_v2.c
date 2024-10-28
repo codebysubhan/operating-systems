907cf2f07188408253502fbb7acfac45191d1a77
codebysubhan <bsdsf22m030@pucit.edu.pk>
2024-10-28 22:38:37 +0500

    adding version 2. Will look for further changes.

--- shell_v2.c
+++ shell_v2.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #define MAX_LEN 512
 #define MAXARGS 10
@@ -13,82 +14,141 @@
 int execute(char* arglist[]);
 char** tokenize(char* cmdline);
 char* read_cmd(char*, FILE*);
-int main(){
-   char *cmdline;
-   char** arglist;
-   char* prompt = PROMPT;   
-   while((cmdline = read_cmd(prompt,stdin)) != NULL){
-      if((arglist = tokenize(cmdline)) != NULL){
-            execute(arglist);
-       //  need to free arglist
-         for(int j=0; j < MAXARGS+1; j++)
-	         free(arglist[j]);
-         free(arglist);
-         free(cmdline);
-      }
-  }//end of while loop
-   printf("\n");
-   return 0;
-}
-int execute(char* arglist[]){
-   int status;
-   int cpid = fork();
-   switch(cpid){
-      case -1:
-         perror("fork failed");
-	      exit(1);
-      case 0:
-	      execvp(arglist[0], arglist);
- 	      perror("Command not found...");
-	      exit(1);
-      default:
-	      waitpid(cpid, &status, 0);
-         printf("child exited with status %d \n", status >> 8);
-         return 0;
-   }
-}
-char** tokenize(char* cmdline){
-//allocate memory
-   char** arglist = (char**)malloc(sizeof(char*)* (MAXARGS+1));
-   for(int j=0; j < MAXARGS+1; j++){
-	   arglist[j] = (char*)malloc(sizeof(char)* ARGLEN);
-      bzero(arglist[j],ARGLEN);
-    }
-   if(cmdline[0] == '\0')//if user has entered nothing and pressed enter key
-      return NULL;
-   int argnum = 0; //slots used
-   char*cp = cmdline; // pos in string
-   char*start;
-   int len;
-   while(*cp != '\0'){
-      while(*cp == ' ' || *cp == '\t') //skip leading spaces
-          cp++;
-      start = cp; //start of the word
-      len = 1;
-      //find the end of the word
-      while(*++cp != '\0' && !(*cp ==' ' || *cp == '\t'))
-         len++;
-      strncpy(arglist[argnum], start, len);
-      arglist[argnum][len] = '\0';
-      argnum++;
-   }
-   arglist[argnum] = NULL;
-   return arglist;
-}      
+int handle_redirection_and_pipes(char* cmdline);
 
-char* read_cmd(char* prompt, FILE* fp){
-   printf("%s", prompt);
-  int c; //input character
-   int pos = 0; //position of character in cmdline
-   char* cmdline = (char*) malloc(sizeof(char)*MAX_LEN);
-   while((c = getc(fp)) != EOF){
-       if(c == '\n')
-	  break;
-       cmdline[pos++] = c;
-   }
-//these two lines are added, in case user press ctrl+d to exit the shell
-   if(c == EOF && pos == 0) 
-      return NULL;
-   cmdline[pos] = '\0';
-   return cmdline;
+int main() {
+    char *cmdline;
+    char *prompt = PROMPT;
+    while ((cmdline = read_cmd(prompt, stdin)) != NULL) {
+        if (handle_redirection_and_pipes(cmdline) != 0) {
+            free(cmdline);
+            continue;
+        }
+        free(cmdline);
+    }
+    printf("\n");
+    return 0;
+}
+
+int handle_redirection_and_pipes(char* cmdline) {
+    int pipefd[2];
+    pid_t pid;
+    int in_fd = 0;
+    char *command = strtok(cmdline, "|");
+
+    while (command != NULL) {
+        char *infile = NULL, *outfile = NULL;
+        char **arglist = tokenize(command);
+
+        // Check for redirection
+        for (int i = 0; arglist[i] != NULL; i++) {
+            if (strcmp(arglist[i], "<") == 0) {
+                infile = arglist[i + 1];
+                arglist[i] = NULL;
+            } else if (strcmp(arglist[i], ">") == 0) {
+                outfile = arglist[i + 1];
+                arglist[i] = NULL;
+            }
+        }
+
+        if (pipe(pipefd) == -1) {
+            perror("Pipe failed");
+            exit(1);
+        }
+
+        pid = fork();
+        if (pid == -1) {
+            perror("Fork failed");
+            exit(1);
+        } else if (pid == 0) { // Child process
+            if (in_fd != 0) { // Redirect stdin
+                dup2(in_fd, 0);
+                close(in_fd);
+            }
+
+            if (infile) { // Input redirection
+                int fd0 = open(infile, O_RDONLY);
+                if (fd0 < 0) {
+                    perror("Error opening input file");
+                    exit(1);
+                }
+                dup2(fd0, 0);
+                close(fd0);
+            }
+
+            if (outfile) { // Output redirection
+                int fd1 = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                if (fd1 < 0) {
+                    perror("Error opening output file");
+                    exit(1);
+                }
+                dup2(fd1, 1);
+                close(fd1);
+            }
+
+            if (command != NULL && strtok(NULL, "|") != NULL) { // Pipe redirection
+                dup2(pipefd[1], 1);
+                close(pipefd[1]);
+            }
+
+            execute(arglist);
+            exit(1);
+        } else { // Parent process
+            waitpid(pid, NULL, 0);
+            close(pipefd[1]);
+            in_fd = pipefd[0];
+            command = strtok(NULL, "|");
+        }
+    }
+
+    return 0;
+}
+
+int execute(char* arglist[]) {
+    execvp(arglist[0], arglist);
+    perror("Command not found...");
+    exit(1);
+}
+
+char** tokenize(char* cmdline) {
+    char** arglist = (char**)malloc(sizeof(char*) * (MAXARGS + 1));
+    for (int j = 0; j < MAXARGS + 1; j++) {
+        arglist[j] = (char*)malloc(sizeof(char) * ARGLEN);
+        bzero(arglist[j], ARGLEN);
+    }
+    if (cmdline[0] == '\0')
+        return NULL;
+    int argnum = 0;
+    char* cp = cmdline;
+    char* start;
+    int len;
+    while (*cp != '\0') {
+        while (*cp == ' ' || *cp == '\t')
+            cp++;
+        start = cp;
+        len = 1;
+        while (*++cp != '\0' && !(*cp == ' ' || *cp == '\t'))
+            len++;
+        strncpy(arglist[argnum], start, len);
+        arglist[argnum][len] = '\0';
+        argnum++;
+    }
+    arglist[argnum] = NULL;
+    return arglist;
+}
+
+char* read_cmd(char* prompt, FILE* fp) {
+    printf("%s", prompt);
+    int c;
+    int pos = 0;
+    char* cmdline = (char*)malloc(sizeof(char) * MAX_LEN);
+    while ((c = getc(fp)) != EOF) {
+        if (c == '\n')
+            break;
+        cmdline[pos++] = c;
+    }
+    if (c == EOF && pos == 0)
+        return NULL;
+    cmdline[pos] = '\0';
+    return cmdline;
 }
